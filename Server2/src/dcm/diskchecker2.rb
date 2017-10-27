37df59b7d92bf447c37f062befe47ae1bd693ba9
Yusuke Haneda <yusukehaneda0824@gmail.com>
2017-10-27 12:43:05 +0900

    diskchecker.rbをhashで返すようにした

--- Server2/src/dcm/diskchecker2.rb
+++ Server2/src/dcm/diskchecker2.rb
@@ -8,7 +8,7 @@
 #
 #require './dcm.rb'
 
-def diskcheck
+#def diskcheck
 
   serve1 = `sshpass -p jiro ssh taro@192.168.0.101 df | grep /$ | awk '{ print \$4 }'`
   serve3 = `sshpass -p jiro ssh taro@192.168.0.103 df | grep /$ | awk '{ print \$4 }'`
@@ -22,23 +22,30 @@ def diskcheck
   max = z  if (z > max)
   result = max
 
+puts serve1,serve3,serve4  
+puts x,y,z  
+puts result
+#puts kvm_1,kvm_3,kvm_4
+
    if result == x then
-     #kvm_1 = Hash.new()
-     kvm_1 = {:hostname => "Server1" , :disk => x}
+     kvm_1 = Hash.new()
+     kvm_1 = {:Server1 => x}
      return kvm_1
    elsif result == y then
-     kvm_3 = {:hostname => "Server3" ,:disk => y}
+     kvm_3 = {:Server3 => y}
      return kvm_3
    else  result == z
-     kvm_4 = {:hostname => "Server4" ,:disk => z}
+     kvm_4 = {:Server4 => z}
      return kvm_4
    end
 
 #end
 #debug用
-  #puts serve1,serve3,serve4  
-  #puts result
-  #puts kvm_1,kvm_3,kvm_4
-  #diskcheck
-end
+puts serve1,serve3,serve4  
+puts x,y,z  
+puts result
+puts kvm_1,kvm_3,kvm_4
+#diskcheck
+#end
 
+#diskcheck
--- Server2/src/dcm/test.rb
+++ Server2/src/dcm/test.rb
@@ -1,3 +1,7 @@
 require "./diskchecker.rb"
 
 puts diskcheck
+kvm = diskcheck
+Hash.new([])
+puts kvm[:hostname]
+puts kvm[:disk]

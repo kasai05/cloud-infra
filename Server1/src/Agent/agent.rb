7d3e56e58b733bc494caf30404124bbaaab0f7dc
a1710dk <a1710dk@aiit.ac.jp>
2017-11-01 00:56:07 +0900

    Update agent.rb

--- Server1/src/Agent/agent.rb
+++ Server1/src/Agent/agent.rb
@@ -1,48 +1,54 @@
-#################################################################
-#
-# RabbitMQからのキューを受け取るプログラム by Kuroki
-# 対象はVMの作成、起動、停止、削除のみ
-# Ver 1.0
-#
-#################################################################
-
-require 'json' 
+require 'json'
 require 'bunny'
-require './VmController.rb' #実際のVM操作を行うクラス
+require './VmController.rb'
+require './QueueSender.rb'
 
-hostname = "localhost" #適宜変更する
-queueName = "kvm1"     #適宜変更する
+ARGV[0].nil? ? HOSTNAME = "127.0.0.1" : HOSTNAME = ARGV[0]
+QUEUENAME = "kvm1"
 
-conn = Bunny.new(:hostname => hostname, :username => "mquser", :password => "mquser")
+conn = Bunny.new(:hostname => HOSTNAME, :username => "mquser", :password => "mquser")
 conn.start
 
 ch = conn.create_channel
-q = ch.queue(queueName)
+q = ch.queue(QUEUENAME)
 
 
 begin
 	vmcon = VmController.new()
+	queueSender = QueueSender.new()
 	puts "[*] Waiting for messages. To exit press CTRL+C"
-	#受信データは、"type","name","uuid","vcpu","memory","disk"
 	q.subscribe(:block => true) do |delivery_info, properties, body|
 		puts "*****データを受信*****"
+		puts "[x]Received " + body
 		hash = JSON.parse(body)
 		#データタイプにより処理振り分け
 		case hash["type"]
 		when "create"
-			vmcon.vmCreate(hash)
+			result = vmcon.vmCreate(hash)
 		when "start"
-			vmcon.vmStart(hash)
+			result = vmcon.vmStart(hash)
 		when "stop"
-			vmcon.vmStop(hash)
+			result = vmcon.vmStop(hash)
 		when "destroy"
-			vmcon.vmDestroy(hash)
+			result = vmcon.vmDestroy(hash)
 		when "delete"
 			vmcon.vmDestroy(hash)
-			vmcon.vmDelete(hash)
+			result = vmcon.vmDelete(hash)
 		else
 			puts "ERROR: Not yet implemented (" + hash["type"] + ")"
+			result = "NotYetImplemented"
 		end
+
+		queueSender.hostname = '192.168.57.10'
+		queueSender.queueName = 'response'
+		
+		hash.store("queueName", "response")
+		hash.store("status", "#{result}")
+		message = hash.to_json
+		queueSender.msg = message
+		queueSender.send()
+		
+
 		puts "[*] Waiting for messages. To exit press CTRL+C"
 	end
 rescue Interrupt => _

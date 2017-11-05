2a55408382c6c63d4e734620ec0670dc901a640f
a1710dk <a1710dk@aiit.ac.jp>
2017-11-06 03:24:10 +0900

    Update VmController.rb
    モジュール化および全体的なリファクタリング。
    エラー処理追加。メタディスク作成プロセス追加。

--- Server1/src/Agent/VmController.rb
+++ Server1/src/Agent/VmController.rb
@@ -1,36 +1,44 @@
-#################################################################
-#								#
-# VMを操作するプログラム By Kuroki				#
-# Ver1.1							#
-#								#
-#################################################################
+# ファイル名：VmController.rb
+# 概要：VirtualMachine操作用モジュール
+# 役割：VirtualMachineを作成/起動/停止/強制停止/削除する
+# 実行方法：メインプログラムから各メソッドを直接呼び出す
+# バージョン：2.0
+# 作成者：黒木
 
-require 'json'
-require 'fileutils'
-require './ModifyXML.rb'
+require 'json'  # json形式とhash形式を相互に変換するライブラリ
+require 'fileutils'  # ファイルコピーなどの処理を行うライブラリ
+require './ModifyXML.rb'  # VirtualMachine定義ファイル(XML)の内容を変更するクラス
+require './MetaCreater.rb'  # メタドライブの作成、内容書き込みを行うモジュール
+require './IpaddrTransfer.rb'  # IPアドレスの0パディングを消去するモジュール
 
-class VmController
-	ORIGINALXML = "/var/kvm/xml/original.xml"
-	NEWXMLPATH = "/var/kvm/xml/"
-	ORIGINALDISK = "/var/kvm/disk/original"
-	NEWDISKPATH = "/var/kvm/disk/"
+module VmController
+	# 定数の設定
+	ORIGINALXML = "/var/kvm/xml/original.xml"  # オリジナルとなるXMLファイル
+	NEWXMLPATH = "/var/kvm/xml/"               # 新しいXMLファイルを作成するパス
+	ORIGINALDISK = "/var/kvm/disk/original"    # オリジナルとなるデータディスク(仮想マシンのHDD)
+	NEWDISKPATH = "/var/kvm/disk/"             # 新しいデータディスクを作成するパス
 
-	def initialize
-	end
-
+	# VirtualMachineを作成するメソッド
 	def vmCreate(hash)
 		begin
-			puts "***** vmの作成開始 *****"
-			newXML = NEWXMLPATH + hash["uuid"] + ".xml"
-			xml = ModifyXML.new(ORIGINALXML, newXML)
-			disksource = NEWDISKPATH + hash["uuid"]
+			puts "$$$$$ vmの作成開始 $$$$$"
 
-			xml.setName(hash["uuid"])
-			xml.setUuid(hash["uuid"])
-			xml.setVcpu(hash["vcpu"])
-			xml.setMemory(hash["memory"])
-			xml.setDiskSource(disksource)
+			puts "***** メタディスクの作成開始 *****"
+			ipaddr = IpaddrTransfer.deletePadding(hash["ipaddr"])  # 192.168.000.001のような形式だとエラーになるので192.168.0.1の形式に変換する
+			devID = MetaCreater.create(hash["uuid"], ipaddr, hash["publickey"])  # メタディスクを作成し、デバイスIDを取得する
+			puts "***** メタディスクの作成完了 *****"
 
+			puts "***** XML作成開始 *****"
+			newXML = NEWXMLPATH + hash["uuid"] + ".xml"  # 新しいXMLファイルのフルパスを設定
+			xml = ModifyXML.new(ORIGINALXML, newXML)  # 新しいXMLファイルを作成
+			disksource = NEWDISKPATH + hash["uuid"]  # 新しいデータディスクのフルパスを設定
+			xml.setName(hash["uuid"])  # xmlで定義する名称をキューメッセージから取得した値(=DataBaseから持って来た値)にする
+			xml.setUuid(hash["uuid"])  # xmlで定義するuuidをキューメッセージから取得した値にする
+			xml.setVcpu(hash["vcpu"])  # 仮想cpuの数をXMLに記載
+			xml.setMemory(hash["memory"])  # 仮想メモリのサイズをXMLに記載
+			xml.setDiskSource(disksource)  # データディスクのフルパスをXMLに記載
+			xml.setMetaDisk(devID)  # メタディスクのデバイスIDをXMLに記載
+			xml.setMacAddress(hash["macaddr"])  # macアドレスをXMLに記載
 			puts "***** XML作成完了 *****"
 
 			#オリジナルディスクからデータをコピーする
@@ -40,17 +48,20 @@ class VmController
 			puts "***** ディスクデータコピー完了 *****"
 
 			#定義ファイルを読み込み
+			puts "***** 定義ファイル読み込み開始 *****"
 			%x[ #{"virsh define " +  newXML} ]
 			puts "***** 定義ファイル読み込み完了 *****"
 			puts "***** vmの作成完了 *****"
 
 			return "created"
-		rescue
+		rescue => error
+			puts error
 			return "error_create"
 		end
 
 	end
 
+	# VirtualMachineを起動するメソッド
 	def vmStart(hash)
 		begin
 			puts "***** vmを起動します。対象uuid : #{hash["uuid"]} *****"
@@ -58,11 +69,13 @@ class VmController
 			value = %x[ #{"virsh start " + target }]
 			puts "***** vmを起動しました。対象uuid : #{hash["uuid"]} *****"
 			return "started"
-		rescue
+		rescue => error
+			puts error
 			return "error_start"
 		end
 	end
 
+	# VirtualMachineを停止するメソッド
 	def vmStop(hash)
 		begin
 			puts "***** vmを停止します。対象uuid : #{hash["uuid"]} *****"
@@ -70,11 +83,13 @@ class VmController
 			value = %x[ #{"virsh shutdown " + target } ]
 			puts "***** vmを停止しました。対象uuid : #{hash["uuid"]} *****"
 			return "stopped"
-		rescue
+		rescue => error
+			puts error
 			return "error_stop"
 		end
 	end
 
+	# VirtualMachineを強制停止するメソッド
 	def vmDestroy(hash)
 		begin
 			puts "***** vmを強制停止します。対象uuid : #{hash["uuid"]} *****"
@@ -82,15 +97,18 @@ class VmController
 			value = %x[ #{"virsh destroy " + target } ]
 			puts "***** vmを強制停止しました。対象uuid : #{hash["uuid"]} *****"
 			return "destroyed"
-		rescue
+		rescue => error
+			puts error
 			return "error_destroy"
 		end
 	end
 
+	# VirtualMachineを削除するメソッド
 	def vmDelete(hash)
 		begin
 			puts "***** vmを削除します。対象uuid : #{hash["uuid"]} *****"
 				target = hash["uuid"]
+			self.vmDestroy(hash)  # 削除前に強制停止を行う
 			value = %x[ #{"virsh undefine " + target } ]
 			targetDisk = NEWDISKPATH + hash["uuid"]
 			File.delete targetDisk
@@ -98,8 +116,11 @@ class VmController
 			File.delete targetFile
 			puts "***** vmを削除しました。対象uuid : #{hash["uuid"]} *****"
 				return "deleted"
-		rescue
+		rescue => error
+			puts error
 			return "error_delete"
 		end
 	end
+
+	module_function :vmCreate, :vmStart, :vmStop, :vmDestroy, :vmDelete
 end

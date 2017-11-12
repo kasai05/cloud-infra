0ae87f3a364f1b9bc4a3275c55d51e83d2b3e47f
a1710dk <a1710dk@aiit.ac.jp>
2017-11-13 00:00:10 +0900

    Create DataBase.rb

---
+++ Server2/src/gui-sinatra/DataBase.rb
@@ -0,0 +1,20 @@
+require 'mysql'
+
+class DataBase
+  
+  def initialize(ip)
+    @hostname = ip 
+    @username = 'root'
+    @password = 'group1'
+    @dbname = 'IkuraCloud'
+  end
+
+  def getVM
+    res = ""
+    client = Mysql.connect(@hostname, @username, @password, @dbname)
+    client.query("SELECT IPaddr FROM VirtualMachine").each do |ans|
+      res += ans.to_s
+    end
+    return res
+  end
+end
